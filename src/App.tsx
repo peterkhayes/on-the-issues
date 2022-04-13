52a6cdc62720913fbc618257a14a106f0030a7c0
Peter Hayes <phayes@figma.com>
2022-04-13 18:43:17 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import classNames from "classnames";
+import styles from "./App.module.css";
+import { useHash } from "react-use";
+import _TOPIC_DATA from "./topics.json";
+import Highlighter from "react-highlight-words";
 
-function App() {
+interface TopicData {
+  topic: string;
+  keywords: string[];
+  exclude?: string[];
+  references: { text: string; keywords: string[] }[];
+}
+
+const TOPIC_DATA: TopicData[] = _TOPIC_DATA;
+
+export default function App() {
+  const [hash] = useHash();
+  const selectedTopicId = hash.replace("#", "");
+  const selectedTopicData = TOPIC_DATA.find(
+    ({ topic }) => getTopicId(topic) === selectedTopicId
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.content}>
+      <header>
+        <h1 className={styles.header}>On The Issues</h1>
+        <div className={styles.topics}>
+          {TOPIC_DATA.map(({ topic }) => {
+            const id = getTopicId(topic);
+            return (
+              <a
+                className={classNames(
+                  styles.topic,
+                  id === selectedTopicId && styles.selectedTopic
+                )}
+                href={`#${id}`}
+              >
+                {topic}
+              </a>
+            );
+          })}
+        </div>
       </header>
+      {selectedTopicData && <Responses topicData={selectedTopicData} />}
     </div>
   );
 }
 
-export default App;
+function Responses({ topicData }: { topicData: TopicData }) {
+  return (
+    <section className={styles.columns}>
+      <div className={styles.column}>
+        <h2 className={styles.header}>Me and My Friends</h2>
+      </div>
+      <div className={styles.column}>
+        <h2 className={styles.header}>
+          <a href="https://www.uschamber.com/" target="_blank" rel="noreferrer">
+            The US Chamber of Commerce
+          </a>
+        </h2>
+        <div className={styles.metadata}>
+          <div className={styles.metadataItem}>
+            <strong>Keywords: </strong>
+            {topicData.keywords.join(", ")}
+          </div>
+          {topicData.exclude && (
+            <div className={styles.metadataItem}>
+              <strong>Excluding: </strong>
+              {topicData.exclude.join(", ")}
+            </div>
+          )}
+        </div>
+        {topicData.references.map(({ text }, i) => (
+          <blockquote key={i}>
+            <Highlighter
+              highlightClassName={styles.keywordMatch}
+              searchWords={topicData.keywords}
+              autoEscape={true}
+              textToHighlight={text}
+            />
+          </blockquote>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+function getTopicId(topic: string) {
+  return topic.toLowerCase().replace(/(\s+|-)/g, "_");
+}
